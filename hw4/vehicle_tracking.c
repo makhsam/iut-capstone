313f9c43ac69962c44f198ffc548dbcaf3cc68e9
Sayidazim <sayidazimahmudov@gmail.com>
2021-04-11 10:06:30 +0500

    create vehicle_tracking.c from hw4_source_2

---
+++ hw4/vehicle_tracking.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define TRIG_PIN 28
+#define ECHO_PIN 29
+
+#define IN1_PIN 1
+#define IN2_PIN 4
+#define IN3_PIN 5
+#define IN4_PIN 6
+
+void initUltrasonic();
+void initDCMotor();
+void goForward();
+void goBackward();
+void stopDCMotor();
+int getDistance();
+
+int main(void)
+{
+    if (wiringPiSetup() == -1)
+        return 0;
+
+    int distance = 0;
+
+    // Init
+    initUltrasonic();
+    initDCMotor();
+
+    // Go forward
+    goForward();
+
+    while (1)
+    {
+        distance = getDistance();
+        printf("distance %dcm\n", distance);
+
+        if (distance > 50)
+        {
+            stopDCMotor();
+            delay(100);
+            goForward();
+        }
+        else if (distance > 20)
+        {
+            stopDCMotor();
+            delay(100);
+            goBackward();
+        }
+        else
+        {
+            break;
+        }
+
+        delay(500);
+    }
+
+    // Stop the Car
+    stopDCMotor();
+    return 0;
+}
+
+/**
+ * Ultrasonic sensor functions
+ */
+void initUltrasonic()
+{
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+
+    // TRIG_PIN must start LOW
+    digitalWrite(TRIG_PIN, LOW);
+    delay(500);
+}
+
+int getDistance()
+{
+    int start_time = 0, end_time = 0;
+    float distance = 0;
+
+    // Send trigger pulse
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    // Wait for echo start
+    while (digitalRead(ECHO_PIN) == 0)
+        start_time = micros(); // Saves the last known time of LOW pulse
+
+    // Wait for echo end
+    while (digitalRead(ECHO_PIN) == 1)
+        end_time = micros(); // Saves the last known time of HIGH pulse
+
+    // Get distance in cm
+    distance = (end_time - start_time) / 29. / 2.;
+
+    return (int)distance;
+}
+
+/**
+ * DC Motor functions
+ */
+void initDCMotor()
+{
+    pinMode(IN1_PIN, OUTPUT);
+    pinMode(IN2_PIN, OUTPUT);
+    pinMode(IN3_PIN, OUTPUT);
+    pinMode(IN4_PIN, OUTPUT);
+
+    digitalWrite(IN1_PIN, HIGH);
+    digitalWrite(IN2_PIN, HIGH);
+    digitalWrite(IN3_PIN, HIGH);
+    digitalWrite(IN4_PIN, HIGH);
+}
+
+void goForward()
+{
+    digitalWrite(IN1_PIN, HIGH);
+    digitalWrite(IN2_PIN, LOW);
+    digitalWrite(IN3_PIN, HIGH);
+    digitalWrite(IN4_PIN, LOW);
+    printf("Forward\n");
+}
+
+void goBackward()
+{
+    digitalWrite(IN1_PIN, LOW);
+    digitalWrite(IN2_PIN, HIGH);
+    digitalWrite(IN3_PIN, LOW);
+    digitalWrite(IN4_PIN, HIGH);
+    printf("Backward\n");
+}
+
+void stopDCMotor()
+{
+    digitalWrite(IN1_PIN, LOW);
+    digitalWrite(IN2_PIN, LOW);
+    digitalWrite(IN3_PIN, LOW);
+    digitalWrite(IN4_PIN, LOW);
+    printf("Stop\n");
+}
